0830066f704d8f6928e0c81a99462b94ff9057f8
Cory Z <54547695+CoryZ182@users.noreply.github.com>
2020-05-20 10:11:19 -0500

    Add files via upload

--- read_from_xml.py
+++ read_from_xml.py
@@ -1,8 +1,9 @@
 from magenta.music import musicxml_reader
 import magenta.music
 import pretty_midi
+from music21 import converter
 
-def read_from_xml(xml_data):
+def read_from_xml(xml_file):
     """Reads an xml file into a prettyMIDI object.
 
     Args:
@@ -12,5 +13,13 @@ def read_from_xml(xml_data):
     Returns:
         a prettyMIDI object
     """
-    xml_note_sequence = musicxml_reader.musicxml_file_to_sequence_proto(xml_data)
+    xml_note_sequence = musicxml_reader.musicxml_file_to_sequence_proto(xml_file)
     return magenta.music.sequence_proto_to_pretty_midi(xml_note_sequence)
+
+def write_to_xml(self, filename):
+    """writes a prettyMIDI object to a musicxml file.
+
+    Args:
+    filename : str
+        path to write xml file to
+    """
